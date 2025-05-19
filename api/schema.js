d581039ce8ffe77a1e337ebe4ace1a99362d9f72
Jamie Degnan <jamie@mondoo.com>
2025-05-19 14:11:29 -0700

    Refactor GraphQL schema: move schema definitions to 'schemas/schema.graphql' and update schema.js to read from the new location

--- api/schema.js
+++ api/schema.js
@@ -6,7 +6,10 @@ import { dirname, join } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const schemaString = readFileSync(join(__dirname, "schema.graphql"), "utf8");
+const schemaString = readFileSync(
+  join(__dirname, "schemas", "schema.graphql"),
+  "utf8"
+);
 
 const typeDefs = gql`
   ${schemaString}
